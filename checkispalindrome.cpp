aec6659b7ca9ad5631b18ec22b093e7644f64e3b
deepakkumar005 <148310007+deepakkumar005@users.noreply.github.com>
2023-10-31 22:43:41 +0530

    Update checkispalindrome.cpp

--- checkispalindrome.cpp
+++ checkispalindrome.cpp
@@ -1,35 +1,33 @@
 //Check if a link list is palindrome or not
-bool isPalindrome(ListNode* head) {
-        ListNode* temp=new ListNode(head->val);
-        ListNode* temp2=head->next;
-        ListNode* orgh=temp;
-        while(temp2!=NULL){
-            temp->next=new ListNode(temp2->val);
-            temp2=temp2->next;
-            temp=temp->next;
-        }
-        ListNode* prev=NULL;
-        ListNode* curr=head;
-        while(curr!=NULL){
-            ListNode* nex=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=nex;
-        }
-        ListNode* newHead=prev;
-        cout<<endl;
-        while(temp!=NULL){
-            cout<<temp->val;
-            temp=temp->next;
-        }
-        cout<<endl;
-        while(orgh!=NULL){
-            
-            if(orgh->val!=newHead->val){
-                return false;
-            }
-            orgh =orgh->next;
-            newHead =newHead->next;
-        }
-        return true;
+#include <iostream>
+using namespace std;
+
+bool palindrome(string s)
+{
+    int i = 0, j = s.size() - 1;
+    while (i <= j)
+    {
+        if (s[i] != s[j])
+            return false;
+        i++;
+        j--;
     }
+    return true;
+}
+int main()
+{
+    string str;
+    cout << "Enter string: ";
+    getline(cin, str);
+
+    if (palindrome(str))
+    {
+        cout << "String is Palindrome";
+    }
+    else
+    {
+        cout << "Not palindrome";
+    }
+
+    return 0;
+}
