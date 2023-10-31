bd942f3f7625abf2bbab631320c14d78058b60b0
deepakkumar005 <148310007+deepakkumar005@users.noreply.github.com>
2023-10-31 22:41:59 +0530

    Update binarysearch.cpp

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,21 +1,48 @@
-//binary search
-int binarySearch(int arr[], int size, int target) {
-    int left = 0;
-    int right = size - 1;
+#include<iostream>
+using namespace std;
 
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
+int binarySearch(int arr[], int size, int key) {
 
-        if (arr[mid] == target) {
-            return mid; 
+    int start = 0;
+    int end = size-1;
+
+    int mid = start + (end-start)/2;
+
+    while(start <= end) {
+
+        if(arr[mid] == key) {
+            return mid;
         }
 
-        if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+        
+        if(key > arr[mid]) {
+            start = mid + 1;
         }
+        else{ 
+            end = mid - 1;
+        }
+
+        mid = start + (end-start)/2;
     }
+    
+    return -1;
+}
 
-    return -1;  
+
+int main() { 
+
+   int n;
+   cout << "enter the no. of elements ";
+    cin >> n;
+   int arr[50];
+   cout << "enter the elements ";
+   for(int i = 0; i < n; i++){
+    cin >> arr[i];
+   }
+   int x;
+   cout << "enter the element to be search ";
+   cin >> x;
+    int ans = binarySearch(arr, n, x);
+    cout << "index of x is " << ans ;
+    return 0;
 }
