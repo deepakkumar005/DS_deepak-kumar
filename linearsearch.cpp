808430bf7fde761f96489b91212c9781b18df289
deepakkumar005 <148310007+deepakkumar005@users.noreply.github.com>
2023-10-31 22:45:52 +0530

    Update linearsearch.cpp

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -1,10 +1,30 @@
 //linear search
-int linearSearch(int arr[], int size, int target) {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == target) {
-            return i; 
-        }
-    }
-    return -1;  
-}
+#include <iosream>
+using namspace std;
 
+int linearSearch(int arr[], int n, int x)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+            return i;
+    }
+    return -1;
+}
+int main() { 
+
+   int n;
+   cout << "enter the no. of elements ";
+    cin >> n;
+   int arr[50];
+   cout << "enter the elements ";
+   for(int i = 0; i < n; i++){
+    cin >> arr[i];
+   }
+   int x;
+   cout << "enter the element to be search ";
+   cin >> x;
+    int ans = linearsearch(arr, n, x);
+    cout << "index of x is " << ans ;
+    return 0;
+}
